11982cc41044ec3e0affaee6d16688d0d5b9d5aa
javadshoja <jbraave@gmail.com>
2023-08-29 12:29:29 +0330

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Navbar from '@/components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -23,6 +25,7 @@ export default function RootLayout({
       className={cn('bg-white text-slate-900 antialiased', inter.className)}
     >
       <body className='min-h-screen bg-slate-50 pt-12 antialiased'>
+        <Navbar />
         <div className='container mx-auto h-full max-w-7xl pt-12'>
           {children}
         </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react'
+import Link from 'next/link'
+
+import { Icons } from './Icons'
+import { buttonVariants } from './ui/Button'
+
+const Navbar = () => {
+  return (
+    <div
+      className='
+        fixed
+        inset-x-0
+        top-0
+        z-10
+        h-fit
+        border-b
+        border-zinc-300
+        bg-zinc-100
+        py-2
+      '
+    >
+      <div
+        className='
+          container
+          mx-auto
+          flex
+          h-full
+          max-w-7xl
+          items-center
+          justify-between
+          gap-2
+        '
+      >
+        <Link href='/' className='flex items-center gap-2'>
+          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+          <p className='hidden text-sm font-medium text-zinc-700 md:block'>
+            Reddit
+          </p>
+        </Link>
+        <Link href='/login' className={buttonVariants()}>
+          Login
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Navbar
