f69c7a941c5271db0ebc40fbc26db6a0c7adb084
javadshoja <jbraave@gmail.com>
2023-08-29 12:27:13 +0330

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,32 @@
-import "./globals.css"
+import { cn } from '@/lib/utils'
 
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] })
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Reddit",
-  description: "A Reddit clone whit Next.js and Typescript.",
+  title: 'Reddit',
+  description: 'A Reddit clone whit Next.js and Typescript.'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang='en'
+      className={cn('bg-white text-slate-900 antialiased', inter.className)}
+    >
+      <body className='min-h-screen bg-slate-50 pt-12 antialiased'>
+        <div className='container mx-auto h-full max-w-7xl pt-12'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
