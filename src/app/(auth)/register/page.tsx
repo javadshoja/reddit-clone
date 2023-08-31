de633987a8d1701b82de986e7612478fe003ef09
javadshoja <jbraave@gmail.com>
2023-08-31 11:37:29 +0330

    feat: add Register

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+
+import { ChevronLeft } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/Button'
+import Register from '@/components/Register'
+
+const RegisterPage = () => {
+  return (
+    <div className='absolute inset-0'>
+      <div
+        className='
+          mx-auto
+          flex
+          h-full
+          max-w-2xl
+          flex-col
+          items-center
+          justify-center
+          gap-20 
+        '
+      >
+        <Link
+          href='/'
+          className={cn(
+            buttonVariants({ variant: 'ghost' }),
+            '-mt-20 self-start'
+          )}
+        >
+          <ChevronLeft className='mr-2 h-4 w-4' />
+          Home
+        </Link>
+
+        <Register />
+      </div>
+    </div>
+  )
+}
+export default RegisterPage
