80920bcabe013b858abc0069d8475b9d55c63073
javadshoja <jbraave@gmail.com>
2023-09-06 13:18:44 +0330

    update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,11 @@ const config = {
         fixStyle: 'inline-type-imports'
       }
     ],
+    '@typescript-eslint/no-unsafe-member-access': ['warn'],
+    '@typescript-eslint/no-unsafe-assignment': ['warn'],
+    '@typescript-eslint/ban-ts-comment': ['warn'],
+
+    // '@typescript-eslint/no-unsafe-return': ['warn'],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@tanstack/query/exhaustive-deps': 'error',
     '@tanstack/query/prefer-query-object-syntax': 'error'
