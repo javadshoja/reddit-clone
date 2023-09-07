d218e76a35ba66e6c902dd6d865dae66b25ab7ae
javadshoja <jbraave@gmail.com>
2023-09-07 13:43:05 +0330

    update(config): add new image domain

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,11 @@ await import('./src/env.mjs')
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com']
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'utfs.io'
+    ]
   }
 }
 
