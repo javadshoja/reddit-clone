9b3fc75a17dc6ed4e737129e7b65895ac5bb5f2b
javadshoja <jbraave@gmail.com>
2023-09-07 13:42:42 +0330

    update(types): add new post type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
-import type { User } from '@/db/schema'
+import type { Comment, Post, Subreddit, User, Vote } from '@/db/schema'
 
 export type SafeUser = Pick<User, 'name' | 'email' | 'image'>
+
+export type ExtendedPost = Post & {
+  author: User
+  subreddit: Subreddit
+  votes: Vote[]
+  comments: Comment[]
+}
