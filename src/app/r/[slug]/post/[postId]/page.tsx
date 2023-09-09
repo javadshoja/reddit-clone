e8e58f1c3e5433ef1f4b55bd77d6f43c77637bec
javadshoja <jbraave@gmail.com>
2023-09-09 13:48:44 +0330

    feat: add post page

---
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,79 @@
+import React, { Suspense } from 'react'
+import { notFound } from 'next/navigation'
+
+import { eq } from 'drizzle-orm'
+
+import type { CachedPost } from '@/types/redis'
+import db from '@/db'
+import { posts, type Post, type User, type Vote } from '@/db/schema'
+import { redis } from '@/lib/redis'
+import { formatTimeToNow } from '@/lib/utils'
+import EditorOutput from '@/components/EditorOutput'
+import PostVoteServer from '@/components/post-vote/PostVoteServer'
+import { PostVoteShell } from '@/components/post-vote/PostVoteShell'
+
+type pageProps = {
+  params: {
+    postId: string
+  }
+}
+
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
+const PostPage: React.FC<pageProps> = async ({ params }) => {
+  const { postId } = params
+
+  const cachedPost = (await redis.hgetall(`post:${postId}`)) as CachedPost
+
+  const post:
+    | (Post & {
+        author: User
+        votes: Vote[]
+      })
+    | undefined = cachedPost
+    ? undefined
+    : await db.query.posts.findFirst({
+        where: eq(posts.id, parseInt(postId)),
+        with: {
+          author: true,
+          votes: true
+        }
+      })
+
+  if (!post && !cachedPost) return notFound()
+
+  const getPostData = async () => {
+    return await db.query.posts.findFirst({
+      where: eq(posts.id, parseInt(postId)),
+      with: {
+        votes: true
+      }
+    })
+  }
+
+  return (
+    <div className='flex h-full flex-col items-center justify-between sm:flex-row sm:items-start'>
+      <Suspense fallback={<PostVoteShell />}>
+        <PostVoteServer
+          postId={post?.id ?? cachedPost.id}
+          getData={getPostData}
+        />
+      </Suspense>
+
+      <div className='w-full flex-1 rounded-sm bg-white p-4 sm:w-0'>
+        <p className='mt-1 max-h-40 truncate text-xs text-gray-500'>
+          Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
+          {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+        </p>
+        <h2 className='py-2 text-xl font-semibold leading-6 text-gray-900'>
+          {post?.title ?? cachedPost.title}
+        </h2>
+
+        <EditorOutput content={post?.content ?? cachedPost.content} />
+      </div>
+    </div>
+  )
+}
+
+export default PostPage
